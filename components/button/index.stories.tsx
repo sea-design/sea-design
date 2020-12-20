8f6b7722b31c3b6e647097c4316f07aa69be3305
Edward Elric <SASUKE688848@gmail.com>
2020-12-20 10:01:26 +0000

    fix: remove backgroud color

--- components/button/index.stories.tsx
+++ components/button/index.stories.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-// also exported from '@storybook/react' if you can deal with breaking changes in 6.1
 import { Story, Meta } from '@storybook/react/types-6-0';
 
 import { Button, ButtonProps } from './index';
@@ -7,9 +6,6 @@ import { Button, ButtonProps } from './index';
 export default {
   title: 'Example/Button',
   component: Button,
-  argTypes: {
-    backgroundColor: { control: 'color' },
-  },
 } as Meta;
 
 const Template: Story<ButtonProps> = args => <Button {...args} />;
