68e16790b299b167c2188e264bb8afd171346190
VictorNanka <VictorNanka@gmail.com>
2020-12-20 18:30:23 +0800

    Revert "Merge branch 'main' of https://github.com/sea-design/sea-design into main"
    This reverts commit c8bf494a64467fa497152a5f6de56c0b59107c6a, reversing
    changes made to da7e2f6f99795524518e2ef208590771f7999aa0.

--- components/button/index.stories.tsx
+++ components/button/index.stories.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+// also exported from '@storybook/react' if you can deal with breaking changes in 6.1
 import { Story, Meta } from '@storybook/react/types-6-0';
 
 import { Button, ButtonProps } from './index';
@@ -6,6 +7,9 @@ import { Button, ButtonProps } from './index';
 export default {
   title: 'Example/Button',
   component: Button,
+  argTypes: {
+    backgroundColor: { control: 'color' },
+  },
 } as Meta;
 
 const Template: Story<ButtonProps> = args => <Button {...args} />;
