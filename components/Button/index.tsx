024441190ac79bec53e0ffc6145e29b52c48e9b3
Murasame <me@murasame.moe>
2020-12-23 15:26:45 +0800

    Add ring for button

--- components/Button/index.tsx
+++ components/Button/index.tsx
@@ -1,16 +1,16 @@
 import {BaseProps} from '../_base/type';
 import {css} from '@emotion/react';
 import styled from '@emotion/styled';
-import tw, {theme} from 'twin.macro';
+import tw, {theme, TwStyle} from 'twin.macro';
 import {FC} from 'react';
 
 type Size = 'default' | 'small' | 'large';
 type Type = 'primary' | 'secondary' | 'tertiary' | 'warning' | 'danger';
-
 export interface ButtonProps extends BaseProps {
   size?: Size;
   type?: Type;
   onClick?: React.MouseEventHandler<HTMLButtonElement>;
+  ring?: boolean;
 }
 
 const SIZE = {
@@ -27,11 +27,28 @@ const TYPE = {
   danger: tw`bg-red-500`,
 };
 
+function RING(ring: boolean): TwStyle {
+  if (ring) {
+    return tw`hover:ring-4`;
+  } else {
+    return tw``;
+  }
+}
+const RING_COLOR = {
+  primary: tw`ring-blue-300`,
+  secondary: tw`ring-blue-300`,
+  tertiary: tw`ring-gray-300`,
+  warning: tw`ring-yellow-300`,
+  danger: tw`ring-red-300`,
+};
+
 export const Button: FC<ButtonProps> = styled.button(
-  ({size = 'default', type = 'primary'}: ButtonProps) => [
-    tw`text-white rounded`,
+  ({size = 'default', type = 'primary', ring = false}: ButtonProps) => [
+    tw`text-white rounded transition-all`,
     TYPE[type],
     SIZE[size],
+    RING_COLOR[type],
+    RING(ring),
     css`
       color: ${theme`colors.white`};
     `,
