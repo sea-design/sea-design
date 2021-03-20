3a5b851a615f37dbabbdc35520f453259d08da2e
Sepush <sepush@outlook.com>
2021-03-20 20:27:28 +0800

    feat:steps progress

--- components/Progress/index.stories.tsx
+++ components/Progress/index.stories.tsx
@@ -74,15 +74,15 @@ export const SmallLine = () => (
 //   </>
 // );
 
-// export const Steps = () => (
-//   <>
-//     <Progress percent={50} steps={3} />
-//     <br />
-//     <Progress percent={30} steps={5} />
-//     <br />
-//     <Progress percent={100} steps={5} size='small' strokeColor='#52c41a' />
-//   </>
-// );
+export const Steps = () => (
+  <>
+    <Progress percent={50} steps={3} />
+    <br />
+    <Progress percent={30} steps={5} />
+    <br />
+    <Progress percent={100} steps={5} size="small" strokeColor="#52c41a" />
+  </>
+);
 
 // export const LinearGradient = () => (
 //   <>
