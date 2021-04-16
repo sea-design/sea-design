cc86569984dc9995732edb729390e1fdd771f571
Sepush <sepush@outlook.com>
2021-04-16 23:10:55 +0800

    fix:fix checkbox can't handle change event

--- components/checkbox/index.tsx
+++ components/checkbox/index.tsx
@@ -1,48 +1,23 @@
-import * as React from 'react';
+import React, {ChangeEventHandler} from 'react';
 import {BaseProps} from '../_base/type';
 import tw, {css, styled} from 'twin.macro';
 
 type Size = 'small' | 'default' | 'large';
 
-// type Color = 'primary' | 'success' | 'warning' | 'secondary' | 'info' | 'error';
-
 export interface CheckboxProps extends BaseProps {
-  // id?: string;
   defaultChecked?: boolean;
   checked?: boolean;
   disabled?: boolean;
-  // isIndeterminate?: boolean;
-  // isInvalid?: boolean;
-  // isRequired?: boolean;
   label?: string;
-  // name?: string;
   value?: string | number;
   size?: Size;
-  // color?: Color;
-  onChange?: React.ChangeEvent<HTMLInputElement>;
+  onChange?: ChangeEventHandler;
 }
 
 const SIZE = {
   small: tw`h-3 w-3`,
   default: tw`h-4 w-4`,
   large: tw`h-6 w-6`,
-};
-// TODO support colors
-// const COLOR = {
-//   'primary': tw`border-indigo-500`,
-//   'success': tw`border-green-500`,
-//   'warning': tw`border-yellow-500`,
-//   'secondary': tw`border-gray-400`,
-//   'info': tw`border-blue-300`,
-//   'error': tw`border-red-500`,
-// }
-
-const checkState = (defaultChecked: boolean) => {
-  if (defaultChecked) {
-    return 'checked';
-  } else {
-    return ``;
-  }
 };
 
 const StyledInput: React.FC<CheckboxProps> = styled.input(
@@ -52,10 +27,11 @@ const StyledInput: React.FC<CheckboxProps> = styled.input(
     disabled = false,
     defaultChecked = false,
   }: CheckboxProps) => [
+    tw`disabled:opacity-50`,
     SIZE[size],
     disabled && `disabled`,
     checked && `checked`,
-    checkState(defaultChecked),
+    defaultChecked && `checked`,
     css`
       border-radius: 0.2em;
       visibility: visible;
@@ -67,11 +43,11 @@ const StyledInput: React.FC<CheckboxProps> = styled.input(
 
 export const Checkbox: React.FC<CheckboxProps> = ({
   defaultChecked,
-  isChecked,
   disabled = true,
   label,
   size,
   value = '',
+  onChange,
 }: CheckboxProps) => {
   return (
     <label>
@@ -79,9 +55,9 @@ export const Checkbox: React.FC<CheckboxProps> = ({
         type="checkbox"
         size={size}
         disabled={disabled}
-        isChecked={isChecked}
         defaultChecked={defaultChecked}
         value={value}
+        onChange={onChange}
       />
       {label !== undefined && <span>{label}</span>}
     </label>
