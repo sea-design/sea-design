d4834ecabf58abe4ef941553851403f284ef8082
Sepush <sepush@outlook.com>
2021-05-07 18:35:06 +0800

    feat:tabs

---
+++ components/Tabs/TabTitle.tsx
@@ -0,0 +1,33 @@
+import {FC, useCallback} from 'react';
+import tw from 'twin.macro';
+import styled from '@emotion/styled';
+
+export interface TabTitleProps {
+  title: string;
+  index: number;
+  setSelectedTab: (index: number) => void;
+  currentActive: number;
+}
+
+export const TabTitle: FC<TabTitleProps> = (props) => {
+  const handleClick = useCallback(() => {
+    props.setSelectedTab(props.index);
+  }, [props.setSelectedTab, props.index]);
+
+  const isActive = (current: number, index: number) => {
+    if (current === index) {
+      return tw`border-0 bg-yellow-100 text-yellow-500`;
+    }
+  };
+
+  const StyledTitle = styled.span((props: TabTitleProps) => [
+    tw` h-8 p-3 flex justify-center text-center items-center`,
+    isActive(props.currentActive, props.index),
+  ]);
+
+  return (
+    <StyledTitle onClick={handleClick} {...props}>
+      {props.title}
+    </StyledTitle>
+  );
+};
